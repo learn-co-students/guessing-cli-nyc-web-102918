9f799ffd0c0918b9443643d74193f9360014691b
Sam B <s77broz@aol.com>
2018-10-15 17:56:25 +0000

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
     if input.to_i == r
       puts "You guessed the correct number!"
     elsif input == "exit"
-    puts "Goodbye!"
+      puts "Goodbye!"
       break
     else
       puts "The computer guessed #{r}."
